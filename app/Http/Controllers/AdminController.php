553110c47e6efb8259f4f8da82def18b2686defb
pratamapujia <pujiistiani123@gmail.com>
2024-04-03 12:24:12 +0700

    admin dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('admin.index');
+        $hariIni = date('Y-m-d');
+
+        $rekapPresensi = DB::table('absensi')
+            ->selectRaw('COUNT(nik) as jmlHadir, SUM(IF(jam_in > "07:00",1,0)) as jmlTerlambat')
+            ->where('tgl_absen', $hariIni)
+            ->first();
+
+        $rekapIzin = DB::table('perizinan')
+            ->selectRaw('SUM(IF(keterangan="i",1,0)) as izin, SUM(IF(keterangan="s",1,0)) as sakit ')
+            ->where('tgl_izin', $hariIni)
+            ->where('laporan', 1)
+            ->first();
+        return view('admin.index', compact('rekapPresensi','rekapIzin'));
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,7 +1,8 @@
 @extends('admin.layouts.index')
 @section('title')
   <title>Dashboard Admin</title>
-  <link rel="stylesheet" href="{{ asset('assets/admin/compiled/css/iconly.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/admin/extensions/@fortawesome/fontawesome-free/css/all.min.css') }}">
+  {{-- <link rel="stylesheet" href="{{ asset('assets/admin/compiled/css/iconly.css') }}"> --}}
 @endsection
 @section('main')
   <div class="page-heading">
@@ -27,18 +28,69 @@
   </div>
   <div class="page-content">
     <div class="row">
-      <div class="col-sm-6 col-md-4 col-lg-3">
+      <div class="col-6 col-sm-6 col-md-6 col-lg-3">
         <div class="card">
           <div class="card-body px-4 py-4-5">
             <div class="row">
-              <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
-                <div class="stats-icon purple mb-2">
-                  <i class="iconly-boldShow"></i>
+              <div class="col-12 col-sm-4 col-md-4 col-lg-5 d-flex justify-content-start ">
+                <div class="stats-icon bg-primary mb-2">
+                  <i class="fas fa-fingerprint"></i>
                 </div>
               </div>
-              <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                <h6 class="text-muted font-semibold">Profile Views</h6>
-                <h6 class="font-extrabold mb-0">112.000</h6>
+              <div class="col-12 col-sm-8 col-md-8 col-lg-7">
+                <h6 class="text-muted font-semibold">Kehadiran</h6>
+                <h6 class="font-extrabold mb-0">{{ $rekapPresensi->jmlHadir }}</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-6 col-sm-6 col-md-6 col-lg-3">
+        <div class="card">
+          <div class="card-body px-4 py-4-5">
+            <div class="row">
+              <div class="col-12 col-sm-4 col-md-4 col-lg-5 d-flex justify-content-start ">
+                <div class="stats-icon bg-danger mb-2">
+                  <i class="fas fa-user-slash"></i>
+                </div>
+              </div>
+              <div class="col-12 col-sm-8 col-md-8 col-lg-7">
+                <h6 class="text-muted font-semibold">Keterlambatan</h6>
+                <h6 class="font-extrabold mb-0">{{ $rekapPresensi->jmlTerlambat }}</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-6 col-sm-6 col-md-6 col-lg-3">
+        <div class="card">
+          <div class="card-body px-4 py-4-5">
+            <div class="row">
+              <div class="col-12 col-sm-4 col-md-4 col-lg-5 d-flex justify-content-start ">
+                <div class="stats-icon bg-secondary mb-2">
+                  <i class="fas fa-clipboard-list"></i>
+                </div>
+              </div>
+              <div class="col-12 col-sm-8 col-md-8 col-lg-7">
+                <h6 class="text-muted font-semibold">Izin</h6>
+                <h6 class="font-extrabold mb-0">{{ $rekapIzin->izin != null ? $rekapIzin->izin : 0}}</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-6 col-sm-6 col-md-6 col-lg-3">
+        <div class="card">
+          <div class="card-body px-4 py-4-5">
+            <div class="row">
+              <div class="col-12 col-sm-4 col-md-4 col-lg-5 d-flex justify-content-start ">
+                <div class="stats-icon bg-warning mb-2">
+                  <i class="fas fa-notes-medical"></i>
+                </div>
+              </div>
+              <div class="col-12 col-sm-8 col-md-8 col-lg-7">
+                <h6 class="text-muted font-semibold">Sakit</h6>
+                <h6 class="font-extrabold mb-0">{{ $rekapIzin->sakit != null ? $rekapIzin->sakit : 0}}</h6>
               </div>
             </div>
           </div>
