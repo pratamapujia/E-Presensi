9a5e6181f6686535c54112976f6a5ae19b1f65bb
pratamapujia <pujiistiani123@gmail.com>
2024-06-09 06:39:29 +0700

    fix icon size

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,8 +10,8 @@
     <title>Login | Absensi Online</title>
     <meta name="description" content="Mobilekit HTML Mobile UI Kit">
     <meta name="keywords" content="bootstrap 4, mobile template, cordova, phonegap, mobile, html" />
-    <link rel="icon" type="image/png" href="{{ asset('assets/img/icon.png') }}">
-    <link rel="apple-touch-icon" href="{{ asset('assets/img/icon.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('assets/img/icon.png') }}" sizes="32x32">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/img/icon.png') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <link rel="manifest" href="{{ asset('__manifest.json') }}">
   </head>
